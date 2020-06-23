a3f7587528465ad8d7160a6ddfb2b00de46947ab
Greg Lim <limjunqu@gmail.com>
2020-06-23 11:24:45 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,48 @@
 import React, { useReducer } from 'react';
 import ToDoList from './ToDoList';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ToDoDetail from './ToDoDetail';
+
+
 const todosInitialState = { 
-  todos:[{ id:'1', text: "finishing writing hooks chapter"},
-    { id:'2', text: "play with kids"},
-    { id:'3', text: "read bible"}
+  todos:[{ id:1, text: "finishing writing hooks chapter"},
+    { id:2, text: "play with kids"},
+    { id:3, text: "read bible"}
   ]
 };
 
 export const TodosContext = React.createContext()
 
-export default function App() {
+const Stack = createStackNavigator();
 
+export default function App (){
   const [state, dispatch] = useReducer(todosReducer,todosInitialState)
 
   return (
-    <TodosContext.Provider value={{state,dispatch}}>      
-      <ToDoList />
+    <TodosContext.Provider value={{state,dispatch}}>  
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="ToDoList">
+          <Stack.Screen name="ToDoList" component={ToDoList} />
+          <Stack.Screen name="ToDoDetail" component={ToDoDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>  
+
     </TodosContext.Provider>    
   )
 }
 
 function todosReducer(state, action){ 
   switch(action.type){ 
-    case 'add':      
+    case 'add':
       // add new todo onto array
       const addedToDos = [...state.todos,action.payload]
       // spread our state and assign todos
-      return {...state,todos:addedToDos} 
+      return {...state,todos:addedToDos}    
+    case 'delete':
+      const filteredTodoState = state.todos.filter( todo => todo.id !== action.payload.id)
+      return {...state, todos: filteredTodoState}
     case 'edit':   
       const updatedToDo = {...action.payload} 
       const updatedToDoIndex = state.todos.findIndex(t => t.id === action.payload.id)
@@ -37,9 +52,6 @@ function todosReducer(state, action){
         ...state.todos.slice(updatedToDoIndex + 1)
       ];
       return {...state, todos: updatedToDos}      
-    case 'delete':
-      const filteredTodoState = state.todos.filter( todo => todo.id !== action.payload.id)
-      return {...state, todos: filteredTodoState}
     default:
       return todosInitialState
   }
